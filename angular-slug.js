2dfea0f2d17e1910f03bc2915383216a4e89aa1a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-04-01 21:56:43 -0300

    Created the wrapper angular module.

---
+++ angular-slug.js
@@ -0,0 +1,25 @@
+/**
+ * Angular Slug
+ * @version v0.0.1
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+angular.module('slug', [])
+
+  /**
+   * Simple slug wrapper as provider.
+   */
+  .provider('slug', function () {
+    slug.$get = function () {
+      return slug;
+    }
+
+    return slug;
+  })
+
+  /**
+   * Slugifies a given string.
+   */
+  .filter('slug', function (slug) {
+    return slug;
+  });
